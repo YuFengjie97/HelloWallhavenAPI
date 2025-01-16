460239a0a3aa52fa3cb49911aca4321455968f87
yufengjie <2457522528@qq.com>
2025-01-16 13:28:52 +0800

    search api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,67 +1,24 @@
 import axios from 'axios'
-import fs, { read } from 'fs-extra'
-import { r } from '../utils';
-import { parse } from 'cookie'
 
-export function readCookie() {
-  try {
-    const isExists = fs.existsSync(r('cookie.txt'))
-    if (isExists) {
-      const res = fs.readFileSync(r('cookie.txt'), 'utf-8')
-      return res
-    } else {
-      return ''
-    }
-  } catch (e) {
-    console.error('读取cookie失败', e)
-    return ''
-  }
-}
-
-export function writeCookie(data: string) {
-  try {
-    fs.writeFileSync(r('cookie.txt'), data, 'utf-8')
-  } catch (e) {
-    console.error('写入cookie失败')
-  }
-}
 
 export const instance = axios.create({
-  headers: {
-    origin: 'https://wallhaven.cc',
-    referer: 'https://wallhaven.cc',
-    'User-Agent': 'Mozilla / 5.0(Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari / 537.36'
-  },
+  baseURL: 'https://wallhaven.cc/api/v1',
   timeout: 10000,
   withCredentials: true
 })
 
-instance.interceptors.request.use((config) => {
-  const cookie = readCookie()
-
-  if (cookie !== '') {
-    config.headers['cookie'] = cookie
-  }
-  return config
-}, (err) => {
-  return Promise.reject(err)
-})
-
-instance.interceptors.response.use((res) => {
-  const setCookie = (res.headers['set-cookie'] ?? []).join(';')
-
-  const cookies = parse(setCookie)
-  writeCookie(`XSRF-TOKEN=${cookies['XSRF-TOKEN']};wallhaven_session=${cookies['wallhaven_session']}`)
-
-  return res;
-}, (err) => {
-  return Promise.reject(err);
-});
-
-export function login(data: { username: string, password: string, _token: string }) {
-  return instance.post('https://wallhaven.cc/auth/login', data)
+export type Params = {
+  q?: string // <keyword> or <id:tagid>
+  apikey?: string
+  categories?: string
+  purity?: string
+  sorting?: 'relevance' | 'random' | 'date_added' | 'views' | 'favorites' | 'toplist' | 'hot'
+  order?: 'asc' | 'desc',
+  ai_art_filter?: string // 0不过滤ai,1过滤ai
 }
 
-export function getPage(url: string) {
-  return instance.get(url)
+export function search(params: Params) {
+  return instance.get('/search', {
+    params,
+  })
 }
--- src/index.ts
+++ src/index.ts
@@ -1,116 +1,35 @@
-import { login, instance, getPage, readCookie } from './api';
-import * as cheerio from 'cheerio';
-import user from '../user.json'
-import readlineSync  from 'readline-sync';
+import { search, type Params } from "./api"
 
-
-type Pic = {
-  previewUrl: string
-  ext: string
-  name: string
-  url: string
-}
-
-
+const apikey = 'yGKH5Hrq3MZAHWiS9VZttLWDUUNxErYU'
 
 /**
- * 在进入login页面时,
- * 1. 从生成的页面中的表单html元素input获取_token
+ * 如果搜索关键字,直接输入
+ * 如果是tagId,输入id:tagId
  */
-async function getToken() {
-  try {
-    const res = await instance.get('https://wallhaven.cc/login')
-    if (res.status === 200) {
-      const html = res.data
+const keyword = 'id:2777'
 
-      const $ = cheerio.load(html)
-      const $token = $('input[name=_token]')
-      const token = $token.attr('value')
-
-      if (!token) {
-        throw Error('login页面不存在token')
-      }
-      return token
-    } else {
-      throw Error('login页面,状态码不为200')
-    }
-  } catch (e) {
-    console.error('login页面,访问失败')
-    throw e
-  }
+/**
+ * categories
+ * |general|anime|people|
+ * |   1   |  0  |   1  |
+ * 
+ * purity
+ * |sfw|sketchy|nsfw|
+ * | 1 |   0   |  1 |
+ */
+const params: Params = {
+  apikey,
+  q: keyword,
+  categories: '111',
+  purity: '111',
+  sorting: 'favorites',
+  order: 'desc',
+  ai_art_filter: '0'
 }
 
-async function goLogin() {
-  // const username = readlineSync.question('input username: ')
-  // const password = readlineSync.question('input password: ')
-
-  const username = user.username
-  const password = user.password
-  const token = await getToken()
-  console.log({ username, password, token });
-
-  try {
-    const res = await login({ username, password, _token: token })
-    if (res.status === 200) {
-      console.log('-------登录成功-------');
-    } else {
-      console.log(`------登录失败--状态码: ${res.status}-----`)
-    }
-  } catch (e) {
-    console.log(`------登录失败------`, e);
-  }
-}
-
-function getPicFromPage(html: string) {
-  const $ = cheerio.load(html)
-  const pics = $('figure.thumb')
-  const res: Pic[] = []
-  pics.each(function () {
-    const previewUrl = $(this).find('a.preview').attr('href') as string
-    const ext = $(this).find('span.png').length > 0 ? 'png' : 'jpg'
-    const hash = (() => {
-      const res = previewUrl?.match(/(?<=\/w\/).*/)
-      if (res) {
-        return res[0]
-      }
-      return ''
-    })()
-    const hashHead = hash.slice(0, 2)
-    let url = previewUrl
-    url = url?.replace(/wallhaven.cc/, 'w.wallhaven.cc')
-    url = url?.replace(/\/w\//, `/full/${hashHead}/wallhaven-`)
-    url += `.${ext}`
-    res.push({
-      previewUrl,
-      ext,
-      name: hash,
-      url,
-    })
-  })
-  return res
-}
-
-
-async function sleep(timeout: number) {
-  return new Promise((resolve, reject) => {
-    setTimeout(resolve, timeout);
-  })
-}
-
-
-; (async () => {
-
-  const reLogin = readlineSync.question('Need RE login? no--0, yes--1 : ')
-  if(reLogin === '1') {
-    await goLogin()
-    await sleep(3000)
-  }
-
-  // const nsfw = 'https://wallhaven.cc/search?categories=111&purity=001&topRange=1y&sorting=toplist&order=desc&ai_art_filter=0&page=1'
-  const nsfw2 = 'https://wallhaven.cc/w/l8leyl'
-  const res = await getPage(nsfw2)
-  console.log(res);
-  
-  // const arr = getPicFromPage(res.data)
-  // console.log(arr);
+;(async() => {
+  const res = await search(params)
+  console.log(res.data.data);
 })()
+
+
